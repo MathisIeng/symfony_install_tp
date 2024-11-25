603d2e24dcd78d79ab91ef7151707c284c4e744e
Tismma <mathis.ieng@lapiscine.pro>
2024-11-25 16:43:29 +0100

    add path function

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,32 +15,32 @@ class ArticleController extends AbstractController
             [
                 'id' => 1,
                 'title' => 'Article 1',
-                'content' => 'Content of article 1',
-                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'content' => 'As de Coeur',
+                'image' => 'https://upload.wikimedia.org/wikipedia/commons/f/fc/01_of_hearts_A.svg',
             ],
             [
                 'id' => 2,
                 'title' => 'Article 2',
-                'content' => 'Content of article 2',
-                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'content' => 'As de pique',
+                'image' => 'https://i.pinimg.com/236x/83/c4/0e/83c40ecd6b38dfb399ad4c892e515036.jpg',
             ],
             [
                 'id' => 3,
                 'title' => 'Article 3',
-                'content' => 'Content of article 3',
-                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'content' => 'As de carreau',
+                'image' => 'https://t3.ftcdn.net/jpg/00/04/08/28/360_F_4082899_Hz0grxupOzcTI71evDTzZkByKnd7Q2yY.jpg',
             ],
             [
                 'id' => 4,
                 'title' => 'Article 4',
-                'content' => 'Content of article 4',
-                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'content' => 'As de trèfle',
+                'image' => 'https://www.tiragecarte.fr/images/cartes-classiques/1-trefle.jpg',
             ],
             [
                 'id' => 5,
                 'title' => 'Article 5',
                 'content' => 'Content of article 5',
-                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'image' => 'https://m.media-amazon.com/images/I/61JAHOF6l1L.jpg',
             ]
 
         ];
